a0877968cae86b644c227e619cf6a817aa7f653e
Septieme <rogerliu00425@gmail.com>
2023-11-08 17:03:17 +0800

    add training

--- utils.py
+++ utils.py
@@ -1,9 +1,56 @@
 from dataset import *
-import logging
 import os
 from tqdm import tqdm
+import torch
+import torch.nn as nn
 
-logger = logging.getLogger(__name__)
+
+def kaiming_init(m):
+    if isinstance(m, nn.Linear):
+        torch.nn.init.kaiming_normal_(m.weight)
+        if m.bias is not None:
+            m.bias.data.fill_(0.01)
+    if isinstance(m, nn.Conv2d):
+        torch.nn.init.kaiming_normal_(m.weight)
+        if m.bias is not None:
+            m.bias.data.fill_(0.01)
+    if isinstance(m, nn.Conv1d):
+        torch.nn.init.kaiming_normal_(m.weight)
+        if m.bias is not None:
+            m.bias.data.fill_(0.01)
+
+
+def power_compress(x):
+    real = x[..., 0]
+    imag = x[..., 1]
+    spec = torch.complex(real, imag)
+    mag = torch.abs(spec)
+    phase = torch.angle(spec)
+    mag = mag**0.3
+    real_compress = mag * torch.cos(phase)
+    imag_compress = mag * torch.sin(phase)
+    return torch.stack([real_compress, imag_compress], 1)
+
+
+def power_uncompress(real, imag):
+    spec = torch.complex(real, imag)
+    mag = torch.abs(spec)
+    phase = torch.angle(spec)
+    mag = mag ** (1.0 / 0.3)
+    real_compress = mag * torch.cos(phase)
+    imag_compress = mag * torch.sin(phase)
+    return torch.stack([real_compress, imag_compress], -1)
+
+
+class LearnableSigmoid(nn.Module):
+    def __init__(self, in_features, beta=1):
+        super().__init__()
+        self.beta = beta
+        self.slope = nn.Parameter(torch.ones(in_features))
+        self.slope.requiresGrad = True
+
+    def forward(self, x):
+        return self.beta * torch.sigmoid(self.slope * x)
 
 def create_dataset():
     # logger.info current path
@@ -16,10 +63,10 @@ def create_dataset():
     mix_dataset = MixSourceDataset(source_datasets)
 
     # logger length
-    logger.info("length of GuitarSetDataset: %s", len(guitarSetDataset))
-    logger.info("length of PhenicxDataset: %s", len(phenicxDataset))
-    logger.info("length of MaestroDataset: %s", len(maestroDataset))
-    logger.info("length of MixSourceDataset: %s", len(mix_dataset))
+    print("length of GuitarSetDataset: %s", len(guitarSetDataset))
+    print("length of PhenicxDataset: %s", len(phenicxDataset))
+    print("length of MaestroDataset: %s", len(maestroDataset))
+    print("length of MixSourceDataset: %s", len(mix_dataset))
 
     return mix_dataset
 
@@ -42,6 +89,6 @@ def generate_samples(dir_path, num, dataset):
 
 if __name__ == "__main__":
     dataset = create_dataset()
-    generate_samples("data/train", 100, dataset=dataset)
+    generate_samples("genData/train", 100, dataset=dataset)
 
 
